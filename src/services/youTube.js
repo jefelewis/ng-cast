af2558ba8619c5a4fd1f460b5ddadea8388dbcd0
jefelewis <jeffylew77@gmail.com>
2018-03-15 11:32:09 -0700

    Adding promises to YouTube services

--- src/services/youTube.js
+++ src/services/youTube.js
@@ -1,17 +1,25 @@
 angular.module('video-player')
-  .service('youTube', function(){
-    $http.get('https://www.googleapis.com/youtube/v3/search', {
-      params: {
-        key: "YOUTUBE_API_KEY",
-        type: 'video',
-        maxResults: '5',
-        q: searchText
-      }
-    }).success( function (data) {
-      if (data.items.length === 0) {
-        $scope.youtubeData = 'No results were found!';
-      }
-      $ctrl.youtubeSearchText = searchText;
-      $ctrl.videos = data.items;
-    });
+  .service('youTube', function($http) {
+    this.search = function (query, callback) {
+      console.log('YOUTUBE IS FIRING');
+      $http({
+        method: 'GET',
+        url: 'https://www.googleapis.com/youtube/v3/search',
+        params: {
+          key: window.YOUTUBE_API_KEY,
+          type: 'video',
+          maxResults: '5',
+          q: query,
+          part: 'snippet',
+        }
+      }).then( function (data) {
+        console.log(callback);
+        callback(data.data.items);
+
+
+      },
+
+      function(data) {console.log('failed', data); });
+
+    };
   });
